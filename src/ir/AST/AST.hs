3d6fb78116df20b33de1e9e10e0dd1d93be2a301
Micael Loberg <micaelloberg@gmail.com>
2017-05-08 19:30:45 +0200

    add methods field to the ADT node

--- src/ir/AST/AST.hs
+++ src/ir/AST/AST.hs
@@ -218,7 +218,8 @@ data ClassDecl = Class {
 data AdtDecl = ADT {
   ameta       :: Meta AdtDecl,
   aname       :: Type,
-  aconstructor :: [AdtConstructor]
+  aconstructor :: [AdtConstructor],
+  amethods     :: [MethodDecl]
 } deriving (Show)
 
 data AdtConstructor = ADTcons {
